e2266b0b02872daee3a4a2a14e28b1d40a8afede
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-06-16 15:23:52 -0600

    Agregado_main

--- ExamenProgramacionFinal/main.cpp
+++ ExamenProgramacionFinal/main.cpp
@@ -1,9 +1,73 @@
 #include <iostream>
+#include<conio.h>
+#include "Persona.h"
+#include "Bitacora.h"
+#include "Puesto.h"
+#include "Usuarios.h"
+#include "menus.h"
+#include "Concepto.h"
+#include "Poliza.h"
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+using std::ios;
+using std::left;
+using std::right;
+using std::fixed;
+using std::showpoint;
 
+#include <ctime>
+#include <vector>
+#include <fstream>
+
+using std::ofstream;
+using std::ostream;
+using std::fstream;
+
+#include <iomanip>
+
+using std::setw;
+using std::setprecision;
+
+#include <cstdlib>
 using namespace std;
 
-int main()
+void portadaproyec();
+//Inicio de main
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
+    menus menuG;
+
+    //Llamado a clase Usuarios para el Login
+    bool accesoUsuarios;
+    portadaproyec();
+    Usuarios usuarioRegistrado;
+    accesoUsuarios=usuarioRegistrado.loginUsuarios();
+    if (accesoUsuarios){menuG.menuGeneral();}
+    system("cls");
+    cout<<"** REGRESE PRONTO **";
     return 0;
+
+}
+void portadaproyec(){
+string line;
+    //char userInput = ' ';
+    ifstream myFile("Portada.txt");
+    if(myFile.is_open())
+    {
+        //Se obtiene el mapa externo y se general el mapa de celdas
+        while( getline(myFile, line))
+        {
+            cout << line << endl;
+        }
+        myFile.close();
+        cout<<"Presione cualquier tecla para continuar";
+        cin.get();
+    }
+    else
+    {
+        cout << "Error FATAL: el archivo de modulo no pudo ser cargado" << endl;
+        cin.get();
+    }
 }
