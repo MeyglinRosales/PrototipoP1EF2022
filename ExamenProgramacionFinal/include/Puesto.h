8135b391b6548c343d515a4711f06294a9b75256
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-06-16 15:55:06 -0600

    Agregado_reportes

--- ExamenProgramacionFinal/include/Puesto.h
+++ ExamenProgramacionFinal/include/Puesto.h
@@ -1,10 +1,26 @@
 #ifndef PUESTO_H
 #define PUESTO_H
-
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
 
 class Puesto
 {
+
     public:
+        string Dpuesto,Crpuesto,Spuesto,Hentrada,Hsalida;
+        void menu ();
+            void Ingresar();
+            void Actualizar ();
+            void Consultar ();
+            void Imprimir ();
+            void Borrar ();
+            void consultarDep();
+
         Puesto();
         virtual ~Puesto();
 
@@ -14,3 +30,4 @@ class Puesto
 };
 
 #endif // PUESTO_H
+
