824eb4fedb3b97793e21d98211c3782f431ad432
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-06-16 15:39:16 -0600

    Agregado_catalogos

---
+++ ExamenProgramacionFinal/include/menus.h
@@ -0,0 +1,21 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+
+class menus
+{
+    public:
+        menus();
+        virtual ~menus();
+        menuGeneral();
+        menuConsultas();
+        menuContabilidad();
+        menuEntrenador();
+        menuSeguridad();
+        int opcion;
+    protected:
+
+    private:
+};
+
+#endif // MENUS_H
