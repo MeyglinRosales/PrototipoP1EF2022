83e8473acf91f5ad12c13cdce64d433757098a13
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-06-16 15:47:27 -0600

    Agregado_Reportes

---
+++ ExamenProgramacionFinal/include/Poliza.h
@@ -0,0 +1,22 @@
+#ifndef POLIZA_H
+#define POLIZA_H
+
+
+class Poliza
+{
+    public:
+        void menu();
+        menuContabilidad();
+        void Consultar();
+
+
+
+        Poliza();
+        virtual ~Poliza();
+
+    protected:
+    private:
+
+};
+
+#endif // POLIZA_H
---
+++ ExamenProgramacionFinal/src/Poliza.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+#include "Poliza.h"
+#include "Bitacora.h"
+#include "Persona.h"
+Poliza::Poliza()
+{
+    //ctor
+}
+
+Poliza::~Poliza()
+{
+    //dtor
+}
+
+
+
+
+void Poliza::menu()
+{
+    Bitacora control;
+    Persona empleado;
+
+    fstream empleadosEntradaSalida = empleado.inicioArchivo();
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+
+	cout<<"\t\t\t................................"<<endl;
+	cout<<"\t\t\t .  Consulta Poliza   ."<<endl;
+	cout<<"\t\t\t................................"<<endl;
+	cout<<"\t\t\t 1. Generacion de poliza "<<endl;
+	cout<<"\t\t\t 2. Salir  "<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"\t\t\t Opcion a escoger:[1/2]"<<endl;
+	cout<<"\t\t\t"<<endl;
+	cout<<"Ingresa tu Opcion: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+
+    case 1:
+        control.nuevaActividadTxt(37);
+        control.nuevaActividad(37);
+        control.fechaHora();
+		empleado.busquedaRegistro2(empleadosEntradaSalida);
+        cout<<"\t\t\t"<<endl;
+        cout << "Presiona Enter para aceptar"<<endl;
+
+		break;
+	case 2:
+	     cout <<"Presiona Enter para salir"<<endl;
+		break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	getch();
+    }while(choice!= 2);
+}
